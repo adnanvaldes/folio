b2960bf5307eb718ece79b1dfff24c7ea85ac294
adnanvaldes <github@terminus.earth>
2025-05-06 14:13:01 -0700

    modify entry logic to avoid db creation on every run

--- folio/main.py
+++ folio/main.py
@@ -3,9 +3,15 @@ import typer
 from db.db import engine, SQLModel, create_db_and_tables
 from books import books
 
-app = typer.Typer()
+app = typer.Typer(no_args_is_help=True)
 app.add_typer(books.app, name="books")
 
-if __name__ == "__main__":
+
+@app.command()
+def init_db():
+    """Initialize database file and create all required tables"""
     create_db_and_tables()
+
+
+if __name__ == "__main__":
     app()
