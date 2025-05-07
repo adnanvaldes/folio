f761cf7342339c387e4bc9e57244b5696f8b07f3
adnanvaldes <github@terminus.earth>
2025-05-07 16:50:45 -0700

    refactor arguments using class inheritance to create consistent interface

--- folio/books/args.py
+++ folio/books/args.py
@@ -23,44 +23,53 @@ class WorkArguments:
     ]
     year = Annotated[
         int,
-        Option(help="Year written (use negative int for BCE)", show_default=False),
+        Option(
+            "--year",
+            "-y",
+            help="Year written (use negative int for BCE)",
+            show_default=False,
+        ),
     ]
-    genre = Annotated[str, Option(help="Genre of the work", show_default=False)]
-    is_read = Annotated[bool, Option(help="Whether the work has been read")]
+    genre = Annotated[
+        str, Option("--genre", "-g", help="Genre of the work", show_default=False)
+    ]
+    is_read = Annotated[
+        bool,
+        Option(
+            "--is-read/--no-read",
+            "-r/-R",
+            help="Filter by read status",
+            show_default=False,
+        ),
+    ]
 
 
 class BookArguments:
     """Class to store argument definitions for book-related commands"""
 
-    pages = Annotated[int, Option(help="Pages in the book", show_default=False)]
+    pages = Annotated[
+        int, Option("--pages", "-p", help="Number of pages", show_default=False)
+    ]
     format = Annotated[
         BookFormat,
         Option(
-            case_sensitive=False, help="Format/medium of the book", show_default=False
+            "--format",
+            "-f",
+            case_sensitive=False,
+            help="Format/medium of the book",
+            show_default=False,
         ),
     ]
     isbn = Annotated[
         str,
-        Option(help="ISBN-13 (or ISBN-10 converted) of the book", show_default=False),
+        Option("--isbn", "-i", help="ISBN-13 of the book", show_default=False),
     ]
     add_book = Annotated[bool, Option(help="Associate work with book a in database")]
 
 
-class SearchArguments:
-    """Class to store argument definitions for search-related commands"""
+class SearchArguments(WorkArguments, BookArguments):
+    """Class to store argument definitions for search-related commands. Inherits all arguments from WorkArguments and BookArguments."""
 
-    title = Annotated[
-        Optional[List[str]],
-        Option("--title", "-t", help="Title of the work", show_default=False),
-    ]
-    author = Annotated[
-        Optional[List[str]],
-        Option("--author", "-a", help="Author of the work", show_default=False),
-    ]
-    year = Annotated[
-        Optional[List[int]],
-        Option("--year", "-y", help="Year written", show_default=False),
-    ]
     year_from = Annotated[
         Optional[int],
         Option(
@@ -73,23 +82,6 @@ class SearchArguments:
             "--year-to", "-yt", help="End year for range search", show_default=False
         ),
     ]
-    genre = Annotated[
-        Optional[List[str]],
-        Option("--genre", "-g", help="Genre of the work", show_default=False),
-    ]
-    is_read = Annotated[
-        Optional[bool],
-        Option(
-            "--is-read/--no-read",
-            "-r/-R",
-            help="Filter by read status",
-            show_default=False,
-        ),
-    ]
-    pages = Annotated[
-        Optional[List[int]],
-        Option("--pages", "-p", help="Number of pages", show_default=False),
-    ]
     pages_min = Annotated[
         Optional[int],
         Option(
@@ -101,20 +93,6 @@ class SearchArguments:
         Option(
             "--pages-max", "-pt", help="Maximum number of pages", show_default=False
         ),
-    ]
-    format = Annotated[
-        Optional[BookFormat],
-        Option(
-            "--format",
-            "-f",
-            case_sensitive=False,
-            help="Format/medium of the book",
-            show_default=False,
-        ),
-    ]
-    isbn = Annotated[
-        Optional[str],
-        Option("--isbn", "-i", help="ISBN-13 of the book", show_default=False),
     ]
     work_id = Annotated[
         Optional[int],
