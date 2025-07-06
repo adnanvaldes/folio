af49f305b33e20c796e92a10de190032085fd63a
adnanvaldes <github@terminus.earth>
2025-07-06 13:01:07 +0200

    add __eq__ method to Work and Book

--- src/folio/models/models.py
+++ src/folio/models/models.py
@@ -17,6 +17,18 @@ class Work(Record["Work"]):
     genre: str | None
     is_read: bool
 
+    def __eq__(self, other):
+        """
+        Two works are equal if they have the same title, author, and year
+        """
+        if not isinstance(other, Work):
+            return False
+        return (
+            self.title.lower() == other.title.lower()
+            and self.author.lower() == other.title.lower()
+            and self.year == other.year
+        )
+
     def __str__(self):
         read = "Read" if self.is_read else "Not read"
         return f"{self.title} by {self.author} (year: {self.year}, {read})"
@@ -28,6 +40,12 @@ class Book(Record["Book"]):
     Represents a specific book instance
     """
 
+    work: Work
     pages: int | None
-    #   format: BookFormat | None
+    format: BookFormat | None
     isbn: str | None
+
+    def __eq__(self, other):
+        if not isinstance(other, Book):
+            return False
+        return self.work == other.work and self.isbn == other.isbn
