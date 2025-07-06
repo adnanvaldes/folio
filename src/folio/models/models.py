fec84a5fd831f4adba1dbd71cc8bced1315c1bf9
adnanvaldes <github@terminus.earth>
2025-07-06 09:33:51 +0200

    update base Record class, include Serializer DI

--- src/folio/models/models.py
+++ src/folio/models/models.py
@@ -3,8 +3,6 @@ from datetime import date
 from typing import Optional, List, Dict, Any
 
 from folio.models.record import Record
-from folio.models.common import ValidationResult
-from folio.services.protocols import Validator, Serializer, Formatter
 
 
 @dataclass
@@ -19,16 +17,6 @@ class Work(Record["Work"]):
     year: int | None
     genre: str | None
     is_read: bool
-
-    validator: Validator
-    serializer: Serializer
-    formatter: Formatter
-
-    def validate(self) -> ValidationResult:
-        return self.validator.validate(self)
-
-    def to_dict(self) -> Dict[str, Any]:
-        return self.serializer.to_dict(self)
 
     def __str__(self):
         read = "Read" if self.is_read else "Not read"
--- src/folio/models/record.py
+++ src/folio/models/record.py
@@ -3,21 +3,36 @@ from dataclasses import dataclass
 from typing import Dict, Any, List, TypeVar, Type
 
 from folio.models.common import ValidationResult
+from folio.services.protocols import Validator, Formatter
+from folio.services.serializers import SerializeStrategy
+
+R = TypeVar("R", bound="Record")
+O = TypeVar("O")
 
 
-class Record[Self: "Record"](ABC):
+class Record[R: "Record"](ABC):
     """
     Abstract base class for all records
     """
 
-    @abstractmethod
-    def validate(self) -> ValidationResult:
-        pass
+    @classmethod
+    def deserialize(cls: R, data: O, serializer: SerializeStrategy[R]) -> R:
+        """
+        Deserialize data into a Record using the provided serializer.
+        """
+        return serializer.deserialize(data, cls)
 
-    @abstractmethod
-    def to_dict(self) -> Dict[str, Any]:
-        """Convert record to dictionary"""
-        pass
+    def serialize(self, serializer: SerializeStrategy[R]) -> O:
+        """
+        Serialize this Record using the provided Serializer
+        """
+        return serializer.serialize(self)
+
+    def validate(self, validator: Validator) -> ValidationResult:
+        """
+        Validate this Record using the provided Validator
+        """
+        return validator.validate(self)
 
     @abstractmethod
     def __str__(self) -> str:
