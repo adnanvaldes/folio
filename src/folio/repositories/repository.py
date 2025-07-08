196650bb07f88edc1fedda3c49f7ff5ab2c5d7fe
adnanvaldes <github@terminus.earth>
2025-07-08 11:52:47 +0200

    create abstract Repository and implement basic InMemoryRepository

--- src/folio/repositories/repository.py
+++ src/folio/repositories/repository.py
@@ -1,59 +1,48 @@
-from typing import List
+from typing import List, Optional, Callable
 from abc import ABC, abstractmethod
+
+from folio.models.record import Record
 
 
 class Repository[R: Record](ABC):
     @abstractmethod
-    def add(self, record: R) -> None: ...
+    def add(self, record: R) -> int:
+        """
+        Add a new record to the repository and return its assigned ID
+        """
+        ...
 
     @abstractmethod
-    def get(self, record_id: int) -> R: ...
-
-    @abstractmethod
-    def list(self) -> List[R]: ...
-
-    @abstractmethod
-    def delete(self, record_id: int) -> None: ...
+    def get(self, id: int) -> Optional[R]:
+        """
+        Retrieve a record by its repository-assigned ID
+        """
+        ...
 
 
-class InMemoryRepository[R: Record](Repository[R]):
+class InMemoryRepository(Repository[R]):
     def __init__(self):
-        self._data: dict[int, R] = {}
-        self._next_id: int = 1
+        self._storage: Dict[int, R] = {}
 
-    def add(self, record: R) -> None:
+    def _make_key(self, record: R) -> int:
         """
-        Add a record to the repository. Assigns an ID if it doesn't have one
-        #TODO: Consider the case when an existing ID is passed.
+        Generate a unique key for the Record
+        using __hash__
         """
-        validation = record.validate()
-        if not validation.is_valid:
-            raise ValueError(f"Invalid record: {', '.join(validation.errors)}")
+        return hash(record)
 
-        if record.id is None:
-            record.id = self._next_id
-            self._next_id += 1
+    def add(self, record: R) -> int:
+        """
+        Add a new record to the repository and return its assigned ID
+        """
+        key = self._make_key(record)
+        if key in self._storage:
+            raise ValueError("Duplicate entry")
+        self_storage[key] = record
+        return key
 
-        self._data[record.id] = record
-
-    def get(self, record_id: int) -> R:
+    def get(self, id: int) -> Optional[R]:
         """
-        Retrieve a record by ID. Raises KeyError if not found
+        Retrieve a record by its repository-assigned ID
         """
-        if record_id not in self._data:
-            raise KeyError(f"Reord with ID {record_id} not found")
-        return self._data[record_id]
-
-    def list(self) -> List[R]:
-        """
-        Return all records as a list
-        """
-        return list(self._data.values())
-
-    def delete(self, record_id: int) -> None:
-        """
-        Delete a record by ID. Raises KeyError if not found.
-        """
-        if record_id not in self._data:
-            raise KeyError(f"Record with ID {record_id} not found")
-        del self._data[record_id]
+        return self._storage.get[id]
