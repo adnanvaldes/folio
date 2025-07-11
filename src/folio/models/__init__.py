ec7acd7517b3c87a6de1dd62a52b58f136806abc
adnanvaldes <github@terminus.earth>
2025-07-11 13:43:54 +0200

    add reasonable import interfaces

---
+++ src/folio/models/__init__.py
@@ -0,0 +1,4 @@
+from .record import Record, R
+from .models import Work, Book, Travel, Address, Employment
+
+__all__ = ["Record", "R", "Work", "Book", "Travel", "Address", "Employment"]
---
+++ src/folio/repositories/__init__.py
@@ -0,0 +1,4 @@
+from .base import Repository
+from .in_memory import InMemoryRepository
+
+__all__ = ["Repository", "InMemoryRepository"]
