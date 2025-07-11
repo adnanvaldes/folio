091570cfc440d89e075f81c49db97bd8b5819822
adnanvaldes <github@terminus.earth>
2025-07-11 12:36:33 +0200

    rewrite tests to use fixtures where possible

--- tests/unit/test_record.py
+++ tests/unit/test_record.py
@@ -5,90 +5,23 @@ from datetime import date, timedelta
 from folio.models.models import Work, Book, Travel, Address, Employment
 
 
-def work_instance(
-    title="Default Title",
-    author="Default Author",
-    year=2020,
-    genre="Fiction",
-    is_read=False,
-) -> Work:
-    return Work(title, author, year, genre, is_read)
-
-
-def book_instance(
-    work: Work | None = None,
-    pages: int | None = 100,
-    format=Book.Format.PRINT,
-    isbn: str | None = "9783161484100",
-) -> Book:
-    return Book(work or work_instance(), pages, format, isbn)
-
-
-def travel_instance(
-    origin="NYC", destination="LON", date=date(2020, 1, 1), notes="Vacation"
-) -> Travel:
-    return Travel(origin=origin, destination=destination, date=date, notes=notes)
-
-
-def employment_instance(
-    start=date(2020, 1, 1),
-    end=None,
-    company="Acme",
-    supervisor="Wild E. Coyote",
-    address="123 Some St",
-    phone="555-1234",
-) -> Employment:
-    return Employment(
-        start=start,
-        end=end,
-        company=company,
-        supervisor=supervisor,
-        address=address,
-        phone=phone,
-    )
-
-
-def address_instance(
-    start=date(2020, 1, 1),
-    end=None,
-    street_address="123 Main",
-    province="ON",
-    country="Canada",
-    postal_code="A1B2C3",
-) -> Address:
-    return Address(
-        start=start,
-        end=end,
-        street_address=street_address,
-        province=province,
-        country=country,
-        postal_code=postal_code,
-    )
-
-
 class TestWork:
-    def test_work_creation(self):
-        work = work_instance(
-            title="Test Book",
-            author="Test Author",
-            year=None,
-            genre=None,
-            is_read=False,
-        )
+    def test_work_creation(self, work_instance):
+        work = work_instance
 
-        assert work.title == "Test Book"
-        assert work.author == "Test Author"
-        assert work.year is None
-        assert work.genre is None
+        assert work.title == "Abril Jacarandil"
+        assert work.author == "Josu Roldan"
+        assert work.year == 2024
+        assert work.genre == "Poetry"
         assert work.is_read is False
 
-    def test_work_identity_and_ordering(self):
-        w1 = work_instance(title="Alpha", author="Anderson", year=1990)
-        w2 = work_instance(
+    def test_work_identity_and_ordering(self, work_factory):
+        w1 = work_factory(title="Alpha", author="Anderson", year=1990)
+        w2 = work_factory(
             title="Alpha", author="Anderson", year=1990, genre="Sci-Fi"
         )  # Same identity
-        w3 = work_instance(title="Beta", author="Anderson", year=1985)
-        w4 = work_instance(title="Alpha", author="Brown", year=2000)
+        w3 = work_factory(title="Beta", author="Anderson", year=1985)
+        w4 = work_factory(title="Alpha", author="Brown", year=2000)
 
         expected_order = [w1, w3, w4]
 
@@ -105,31 +38,30 @@ class TestWork:
 
 
 class TestBook:
-    def test_book_creation(self):
-        work = work_instance()
-        book = book_instance(work)
+    def test_book_creation(self, book_instance):
+        book = book_instance
 
-        assert book.work.title == "Default Title"
-        assert book.work.author == "Default Author"
-        assert book.pages == 100
+        assert book.work.title == "Abril Jacarandil"
+        assert book.work.author == "Josu Roldan"
+        assert book.pages == 127
         assert book.format == Book.Format.PRINT
-        assert book.isbn == "9783161484100"
+        assert book.isbn == "9786079818180"
 
-    def test_book_identity_and_ordering(self):
-        work1 = work_instance(title="Alpha", author="Anderson", year=1990)
-        work2 = work_instance(title="Beta", author="Brown", year=2000)
+    def test_book_identity_and_ordering(self, book_factory, work_factory):
+        work1 = work_factory(title="Alpha", author="Anderson", year=1990)
+        work2 = work_factory(title="Beta", author="Brown", year=2000)
 
-        b1 = book_instance(work=work1, format=Book.Format.AUDIO, isbn="111", pages=300)
-        b2 = book_instance(
+        b1 = book_factory(work=work1, format=Book.Format.AUDIO, isbn="111", pages=300)
+        b2 = book_factory(
             work=work1, format=Book.Format.AUDIO, isbn="111", pages=400
         )  # Same identity
-        b3 = book_instance(
+        b3 = book_factory(
             work=work1, format=Book.Format.EBOOK, isbn="111", pages=300
         )  # Diff format
-        b4 = book_instance(
+        b4 = book_factory(
             work=work1, format=Book.Format.PRINT, isbn="222", pages=300
         )  # Diff ISBN
-        b5 = book_instance(
+        b5 = book_factory(
             work=work2, format=Book.Format.PRINT, isbn="111", pages=300
         )  # Diff Work
 
@@ -148,28 +80,24 @@ class TestBook:
 
 
 class TestTravel:
-    def test_travel_creation(self):
-        travel = travel_instance(
-            origin="NYC", destination="LON", date=date(2020, 1, 1), notes="Vacation"
-        )
+    def test_travel_creation(self, travel_instance):
+        travel = travel_instance
 
         assert travel.origin == "NYC"
         assert travel.destination == "LON"
         assert travel.date == date(2020, 1, 1)
         assert travel.notes == "Vacation"
 
-    def test_travel_identity_and_ordering(self):
-        t1 = travel_instance(origin="NYC", destination="LON", date=date(2020, 1, 1))
-        t2 = travel_instance(
+    def test_travel_identity_and_ordering(self, travel_factory):
+        t1 = travel_factory(origin="NYC", destination="LON", date=date(2020, 1, 1))
+        # Same identity
+        t2 = travel_factory(
             origin="NYC", destination="LON", date=date(2020, 1, 1), notes="Business"
-        )  # Same identity
-        t3 = travel_instance(
-            origin="PAR", destination="ROM", date=date(2021, 1, 1)
-        )  # Diff identity
+        )
+        # Diff identity
+        t3 = travel_factory(origin="PAR", destination="ROM", date=date(2021, 1, 1))
 
         expected_order = [t1, t3]
-
-        # Ordering
         travels = [t3, t1]
         assert sorted(travels) == expected_order
 
@@ -181,26 +109,19 @@ class TestTravel:
 
 
 class TestAddress:
-    def test_address_creation(self):
-        addr = address_instance(
-            start=date(2020, 1, 1),
-            end=None,
-            street_address="123 Main St",
-            province="ON",
-            country="Canada",
-            postal_code="A1B2C3",
-        )
+    def test_address_creation(self, address_instance):
+        addr = address_instance
 
         assert addr.start == date(2020, 1, 1)
         assert addr.end is None
-        assert addr.street_address == "123 Main St"
+        assert addr.street_address == "123 Main"
         assert addr.province == "ON"
         assert addr.country == "Canada"
         assert addr.postal_code == "A1B2C3"
         assert isinstance(addr.duration, timedelta)
 
-    def test_address_identity_and_ordering(self):
-        a1 = address_instance(
+    def test_address_identity_and_ordering(self, address_factory):
+        a1 = address_factory(
             start=date(2020, 1, 1),
             end=date(2022, 1, 1),
             street_address="123 Main St",
@@ -208,7 +129,7 @@ class TestAddress:
             country="Canada",
             postal_code="A1B2C3",
         )
-        a2 = address_instance(
+        a2 = address_factory(
             start=date(2018, 1, 1),
             end=date(2019, 1, 1),
             street_address="123 Main St",
@@ -216,7 +137,7 @@ class TestAddress:
             country="Canada",
             postal_code="A1B2C3",
         )
-        a3 = address_instance(
+        a3 = address_factory(
             start=date(2021, 6, 1),
             end=None,
             street_address="456 Elm St",
@@ -238,15 +159,8 @@ class TestAddress:
 
 
 class TestEmployment:
-    def test_employment_creation(self):
-        emp = employment_instance(
-            start=date(2020, 1, 1),
-            end=None,
-            company="Acme",
-            supervisor="Wild E. Coyote",
-            address="123 Some St",
-            phone="555-1234",
-        )
+    def test_employment_creation(self, employment_instance):
+        emp = employment_instance
 
         assert emp.start == date(2020, 1, 1)
         assert emp.end is None
@@ -255,10 +169,10 @@ class TestEmployment:
         assert emp.address == "123 Some St"
         assert emp.phone == "555-1234"
 
-    def test_employment_identity_and_ordering(self):
-        e1 = employment_instance(start=date(2025, 1, 1), company="Acme")
-        e2 = employment_instance(start=date(1900, 2, 2), company="Acme")
-        e3 = employment_instance(
+    def test_employment_identity_and_ordering(self, employment_factory):
+        e1 = employment_factory(start=date(2025, 1, 1), company="Acme")
+        e2 = employment_factory(start=date(1900, 2, 2), company="Acme")
+        e3 = employment_factory(
             start=date(2020, 1, 1),
             end=date(2025, 1, 1),
             company="Megacorp",
