74a582e437d32b962590eb19687da7b3ee078368
adnanvaldes <github@terminus.earth>
2025-07-12 19:50:00 +0200

    add service layer stubs

---
+++ src/folio/services/__init__.py
@@ -0,0 +1,13 @@
+from .address_service import AddressService
+from .book_service import BookService
+from .employment_service import EmploymentService
+from .travel_service import TravelService
+from .work_service import WorkService
+
+__all__ = {
+    "AddressService",
+    "BookService",
+    "EmploymentService",
+    "TravelService",
+    "WorkService",
+}
---
+++ src/folio/services/book_service.py
@@ -0,0 +1,10 @@
+from folio.repositories import BookRepository
+from folio.models import Book
+
+from .work_service import WorkService
+
+
+class BookService:
+    def __init__(self, repository: BookRepository, work_service: WorkService): ...
+    def add(self, Book: Book) -> int: ...
+    def list(self) -> list[Book]: ...
