212559a6d3198bf72b239c3529435beac04d8360
adnanvaldes <github@terminus.earth>
2025-07-20 20:36:30 +0200

    implement basic travel service

--- src/folio/services/travel_service.py
+++ src/folio/services/travel_service.py
@@ -1,8 +1,23 @@
-from folio.repositories import TravelRepository
+from datetime import date
+
 from folio.models import Travel
+from folio.uow import UnitOfWork
 
 
 class TravelService:
-    def __init__(self, repository: TravelRepository): ...
-    def add(self, travel: Travel) -> int: ...
-    def list(self) -> list[Travel]: ...
+    def __init__(self, uow: UnitOfWork):
+        self.uow = uow
+
+    def add(self, origin: str, destination: str, date: date, notes: str = ""):
+        travel = Travel(origin=origin, destination=destination, date=date, notes=notes)
+
+        with self.uow:
+            uow.travel.add(travel)
+
+    def get(self, travel_id):
+        with self.uow:
+            return uow.travel.get(travel_id)
+
+    def list(self) -> list[Travel]:
+        with self.uow:
+            return uow.travel.list()
