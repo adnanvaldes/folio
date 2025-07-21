6df9a8d3fd332c6f0cec34bc819c4d60f14e9508
adnanvaldes <github@terminus.earth>
2025-07-21 21:14:46 +0200

    add Book formats to folio.models namespace

--- src/folio/models/__init__.py
+++ src/folio/models/__init__.py
@@ -1,8 +1,20 @@
 from .work import Work
-from .book import Book
+from .book import Book, FormatType, BookFormat, TextFormat, AudioFormat
 from .travel import Travel
 from .address import Address
 from .employment import Employment
 from .record import Record, R
 
-__all__ = ["Work", "Book", "Travel", "Address", "Employment", "Record", "R"]
+__all__ = [
+    "Work",
+    "Book",
+    "Travel",
+    "Address",
+    "Employment",
+    "Record",
+    "R",
+    "FormatType",
+    "BookFormat",
+    "TextFormat",
+    "AudioFormat",
+]
